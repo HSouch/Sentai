2d606500b309be3edeb59da33633ee1c65f55b0d
DESKTOP-3DLAU7V\Harrison <hsouchereau@outlook.com>
2018-08-09 21:05:16 -0300

    Added Fundamental Units

--- sentai/units.py
+++ sentai/units.py
@@ -2,38 +2,60 @@
 Units for sentai and related applications. All units are derived from SI units.
 """
 
+# Constants
+p_length = 1.616229e-35         # Planck Length (in m)
+p_mass = 2.176e-8               # Planck Mass (in kg)
+p_time = 5.3912e-44             # Planck time (in s)
+
+c = 2.99782e8                   # Speed of light (m/s)
+g = 6.67408e-11                 # Gravitational Constant (m^3 * kg^-1 * s^-2)
+h = 6.6261e-34                  # Planck Constant (J * s)
+h_bar = 1.0546e-34              # Reduced Planck Constant (J * s)
+
+k = 8.987552e9                  # Coulomb's Constant (1/(4 * pi * epsilon_naught))
+c_const = 8.987552e9            # Coulomb's Constant (1/(4 * pi * epsilon_naught))
+
+sb = 5.670367e-8                # Stefan-Boltzmann Constant
+stef_boltz = 5.670367e-8        # Stefan-Boltzmann Constant
+
+
+# Fundamental Units of Mass
+m_electron = 9.109384e-31       # Mass of Electron
+m_proton = 1.672622e-27         # Mass of Proton
+m_neutron = 1.674927e-27        # Mass of Neutron
+
 
 # Distance
-ang = 1e-10
-nm = 1e-9
-mm = 1e-3
-km = 1000
-au = 149597870700
-ly = 9.4607e15
-pc = 3.0857e16
+ang = 1e-10                     # Angstrom
+nm = 1e-9                       # Nanometre
+mm = 1e-3                       # Millimetre
+km = 1000                       # Kilometre
+au = 149597870700               # Astronomical Unit
+ly = 9.4607e15                  # Light Year
+pc = 3.0857e16                  # Parsec
 
 
 # Time
-min = 60
-hour = 3600
-yr = 3.154e+7
+minute = 60                        # Minute
+hour = 3600                     # Hour
+yr = 3.154e+7                   # Year
 
 
-# Mass
-m_sun = 1.9885e30
+# Celestial Mass
+m_sun = 1.9885e30               # Solar Mass
 
-m_mercury = 3.30e23
-m_venus = 4.87e24
-m_earth = 5.97e24
-m_moon = 7.3e22
-m_mars = 6.42e23
-m_jupiter = 1.898e27
-m_saturn = 5.68e26
-m_uranus = 8.68e25
-m_neptune = 1.02e26
-m_pluto = 1.46e22
+m_mercury = 3.30e23             # Mass of Mercury
+m_venus = 4.87e24               # Mass of Venus
+m_earth = 5.97e24               # Mass of Earth
+m_moon = 7.3e22                 # Mass of Earth's moon
+m_mars = 6.42e23                # Mass of Mars
+m_jupiter = 1.898e27            # Mass of Jupiter
+m_saturn = 5.68e26              # Mass of Saturn
+m_uranus = 8.68e25              # Mass of Uranus
+m_neptune = 1.02e26             # Mass of Neptune
+m_pluto = 1.46e22               # Mass of Pluto
 
 
-# Radii
+# Celestial Radii
 r_sun = 6.955e8
 e_earth = 6.3781e6
