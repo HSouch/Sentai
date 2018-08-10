6cc924095f60aef1de2044e8ab59cf679c5f3366
DESKTOP-3DLAU7V\Harrison <hsouchereau@outlook.com>
2018-08-09 21:06:42 -0300

    Added simulation stubs

---
+++ sentai/simulation/nbodysim.py
@@ -0,0 +1,15 @@
+"""
+Class containing n-body simulation methods
+"""
+
+
+def celestial_body_sim(bodies, timestep, sim_time):
+    """
+    Iterates over all bodies, processes their acceleration and adjusts positions over a given timestep. Runs until time
+    reaches the simulation time. Returns a list containings lists of position Vector3 objects for each body.
+    :param bodies:
+    :param timestep:
+    :param sim_time:
+    :return:
+    """
+    return None
---
+++ sentai/simulation/random.py
@@ -0,0 +1,3 @@
+"""
+Random Generation Methods for N-Body simulation.
+"""
