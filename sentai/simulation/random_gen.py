daf4852f2d5d3d12d7af4ea9a644f3a8ecfb3c59
DESKTOP-3DLAU7V\Harrison <hsouchereau@outlook.com>
2018-08-22 01:39:15 -0300

    Adjustments

--- sentai/simulation/random_gen.py
+++ sentai/simulation/random_gen.py
@@ -1,5 +1,5 @@
 import random
-from sentai.vectors.Vector3 import *
+# from sentai.vectors.Vector3 import *
 from sentai.dynamics.DynamicBody import *
 
 """
@@ -7,15 +7,24 @@ Random Generation Methods for N-Body simulation.
 """
 
 
-def generate_random_bodies(num_bodies, max_dist, max_velocity, min_mass, max_mass):
+def generate_random_bodies(num_bodies: int, max_dist: float, max_velocity: float, min_mass: float, max_mass: float):
+    """
+    Create a list of bodies with specified locations, velocities, and masses.
+    :param num_bodies:
+    :param max_dist:
+    :param max_velocity:
+    :param min_mass:
+    :param max_mass:
+    :return:
+    """
     bodies = []
 
     for n in range(0, num_bodies):
-        dist = Vector3(random.uniform(-max_dist, max_dist), random.uniform(-max_dist, max_dist),
-                       random.uniform(-max_dist, max_dist))
+        pos = Vector3(random.uniform(-max_dist, max_dist), random.uniform(-max_dist, max_dist),
+                      random.uniform(-max_dist, max_dist))
         vel = Vector3(random.uniform(-max_velocity, max_velocity), random.uniform(-max_velocity, max_velocity),
                       random.uniform(-max_velocity, max_velocity))
         mass = random.uniform(min_mass, max_mass)
-        bodies.append(DynamicBody(dist, vel, mass))
+        bodies.append(DynamicBody(pos, vel, mass))
 
     return bodies
