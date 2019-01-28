4148d5d0a92eb330aa40980714eef0134ac087ab
HSouch <hsouchereau@outlook.com>
2019-01-28 12:00:02 -0400

    Moved files into proper directory

--- Tests.py
+++ Tests.py
@@ -5,8 +5,3 @@ from sentai.astro import *
 #     bodies = random_gen.generate_random_bodies(10000, 5 * units.au, 40 * units.km, 1 * units.m_earth, 4 * units.m_sun)
 #     nbodysim.advanced_celestial_body_sim(bodies, 1, 1)
 
-energies = [0.02, 0.2, 2, 20]
-
-for e in energies:
-    print(e, photoelectric_absorption(e), mean_free_path(photoelectric_absorption(e), 1),
-          mean_free_path(photoelectric_absorption(e), 1) * units.cm / units.pc)
