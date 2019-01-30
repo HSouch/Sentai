8d428e0241b806c9e2860ba65e2d1e6d6c684101
HSouch <hsouchereau@outlook.com>
2019-01-29 23:12:20 -0400

    Added coulomb forces

--- dynamics/__init__.py
+++ dynamics/__init__.py
@@ -1,1 +1,4 @@
 # General Dynamics (Colliding bodies, etc...)
+
+from .DynamicBody import *
+from .forces import *
--- dynamics/forces.py
+++ dynamics/forces.py
@@ -1,6 +1,6 @@
-import units
-from dynamics.DynamicBody import DynamicBody
-from vectors.Vector3 import Vector3
+from sentai import units
+from sentai.dynamics.DynamicBody import *
+from sentai.vectors.Vector3 import Vector3
 
 
 def grav_force(db1: DynamicBody, db2: DynamicBody):
@@ -25,9 +25,36 @@ def grav_acceleration(db1: DynamicBody, db2: DynamicBody):
 
 
 def calc_net_grav_acceleration(bodies, x):
+    """
+    Calculates the net gravitational acceleration for the xth particle in a list of Dynamic Bodies.
+    :param bodies:
+    :param x:
+    :return:
+    """
     net_acceleration = Vector3(0, 0, 0)
     for y in range(0, len(bodies)):
         if y == x:
             continue
         net_acceleration += grav_acceleration(bodies[x], bodies[y])
     return net_acceleration
+
+
+def coulomb_force(db1: Molecule, db2: Molecule):
+    """
+    Calculates the dynamic force between two dynamic bodies, assumed to be atoms affected by the Coulomb force
+    :param db1:
+    :param db2:
+    :return:
+    """
+    distance_vector = db2.position - db1.position
+    try:
+        force = units.c_const * db1.charge * db2.charge / (distance_vector.map ** 2)
+    except ZeroDivisionError:
+        return Vector3(0, 0, 0)
+    return distance_vector.as_unit() * force
+
+
+def coulombic_acceleration(db1: Molecule, db2: Molecule):
+    force = coulomb_force(db1, db2)
+    return force / db1.mass
+
